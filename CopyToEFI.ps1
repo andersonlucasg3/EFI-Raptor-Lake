ab2df91a0706e7c5939f5dbb949f3a3b31fe43b1
Anderson Lucas C. Ramos <andersonlcramos@icloud.com>
2025-09-16 12:31:55 -0300

    Updated script to work on windows

--- CopyToEFI.ps1
+++ CopyToEFI.ps1
@@ -1,31 +1,58 @@
 #!/usr/local/microsoft/powershell/7/pwsh
 
-$MountPoint = "/Volumes/EFI"
-$SystemEFIPath = "$MountPoint/EFI"
-$SystemEFIBootPath = "$SystemEFIPath/BOOT"
-$SystemEFIOCPath = "$SystemEFIPath/OC"
-
-if (-not (Test-Path $SystemEFIPath))
+if ($IsMacOS)
 {
-    $AvailableDisks = $(diskutil list | grep EFI) | Out-String
-
-    $DiskId = Read-Host -Prompt "Please provide an EFI disk to mount:`n$AvailableDisks"
-
-    if ([string]::IsNullOrEmpty($DiskId))
+    $MountPoint = "/Volumes/EFI"
+    $SystemEFIPath = "$MountPoint/EFI"
+    $SystemEFIBootPath = "$SystemEFIPath/BOOT"
+    $SystemEFIOCPath = "$SystemEFIPath/OC"
+}
+else
+{
+    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
     {
-        Write-Error "EFI volume not mounted. Please mount the EFI volume and try again."
+        Write-Host "Restarting script as Administrator..."
+
+        pwsh -Command "Start-Process pwsh -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""$PSCommandPath""' -Verb RunAs"
 
         return
     }
 
-    if (-not (Test-Path $MountPoint))
-    {
-        sudo pwsh -c "New-Item -Path '$MountPoint' -ItemType Directory"
-    }
+    $SystemEFIPath = "Z:\EFI"
+    $SystemEFIBootPath = "$SystemEFIPath\BOOT"
+    $SystemEFIOCPath = "$SystemEFIPath\OC"
+}
 
-    sudo diskutil mount -mountPoint $MountPoint $DiskId
-    
-    if (-not $? -or -not (Test-Path $SystemEFIPath))
+if (-not (Test-Path $SystemEFIPath))
+{
+    if ($IsMacOS)
+    {
+        $AvailableDisks = $(diskutil list | grep EFI) | Out-String
+
+        $DiskId = Read-Host -Prompt "Please provide an EFI disk to mount:`n$AvailableDisks"
+
+        if ([string]::IsNullOrEmpty($DiskId))
+        {
+            Write-Error "EFI volume not mounted. Please mount the EFI volume and try again."
+
+            return
+        }
+
+        if (-not (Test-Path $MountPoint))
+        {
+            sudo pwsh -c "New-Item -Path '$MountPoint' -ItemType Directory"
+        }
+
+        sudo diskutil mount -mountPoint $MountPoint $DiskId
+        
+        if (-not $? -or -not (Test-Path $SystemEFIPath))
+        {
+            Write-Error "EFI volume not mounted. Please mount the EFI volume and try again."
+
+            return
+        }
+    }
+    else
     {
         Write-Error "EFI volume not mounted. Please mount the EFI volume and try again."
 
@@ -37,14 +64,14 @@ if (Test-Path $SystemEFIBootPath)
 {
     Write-Host "Removing $SystemEFIBootPath"
 
-    rm -rf "$SystemEFIBootPath"
+    Remove-Item "$SystemEFIBootPath" -Recurse -Force
 }
 
 if (Test-Path $SystemEFIOCPath)
 {
     Write-Host "Removing $SystemEFIOCPath"
     
-    rm -rf "$SystemEFIOCPath"
+    Remove-Item "$SystemEFIOCPath" -Recurse -Force
 }
 
 Write-Host "Copying EFI to $SystemEFIPath"
